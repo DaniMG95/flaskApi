de50b11ec33e8405fa3299bb8c179fa1fb6abab9
DaniMG95 <dani_16_5_95@hotmail.com>
2021-10-04 13:17:14 +0200

    Get and delete users

--- app.py
+++ app.py
@@ -4,12 +4,13 @@ from flask_jwt import JWT
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 from security import authenticate, identity
-from resources.user import UserRegister
+from resources.user import UserRegister, User
 import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///C:\\Users\\dani_\\Documents\\flask\\section6\\code\\data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'Jose'
 api = Api(app)
 
@@ -21,6 +22,7 @@ api.add_resource(ItemList, '/items')
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
 api.add_resource(UserRegister, '/register')
+api.add_resource(User, '/user/<int:user_id>')
 
 if __name__ == '__main__':
     from db import db
